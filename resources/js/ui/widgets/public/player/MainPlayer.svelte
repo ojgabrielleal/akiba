56ff9f4b259bc5e725b9ffc9a5a21f244759ad1f
ojgabrielleal <joaogabrieel79@gmail.com>
2026-06-05 23:35:36 -0300

    Refactor code structure for improved readability and maintainability

--- resources/js/ui/widgets/public/player/MainPlayer.svelte
+++ resources/js/ui/widgets/public/player/MainPlayer.svelte
@@ -25,12 +25,12 @@
             music: stream.current_song.music,
         },
         phrase: {
-            text: air.phrase.text,
-            icon: air.phrase.icon ?? locutionIcons[17].url,
-            texture: air.phrase.texture ?? locutionTextures[0].url,
+            text: air[0].phrase.text,
+            icon: air[0].phrase.icon ?? locutionIcons[17].url,
+            texture: air[0].phrase.texture ?? locutionTextures[0].url,
             decoration: {
-                left: air.phrase.decoration?.left ?? locutionDecorations[0].left,
-                right: air.phrase.decoration?.right ?? locutionDecorations[0].right,
+                left: air[0].phrase.decoration?.left ?? locutionDecorations[0].left,
+                right: air[0].phrase.decoration?.right ?? locutionDecorations[0].right,
             },
         },
     };
