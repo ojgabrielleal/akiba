a01a219a0fe46d0e592d5e71215c71da842e78a5
ojgabrielleal <joaogabrieel79@gmail.com>
2026-06-06 16:06:52 -0300

    Integrate OneSignal service worker and update app configuration for notifications

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,9 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker ativado');
+self.addEventListener("activate", event => {
+  console.log("Service Worker ativado");
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,10 @@
         OneSignalDeferred.push(async function (OneSignal) {
             await OneSignal.init({
                 appId: "097a886d-f9b7-46b1-9827-03fea8d9897b",
+                serviceWorkerPath: "/sw.js",
+                serviceWorkerParam: {
+                    scope: "/"
+                }
             });
         });
     </script>
