d1d5e54a6709c60ad88e958f844562f29c310946
ojgabrielleal <joaogabrieel79@gmail.com>
2026-06-06 23:33:28 -0300

    style: format HTML file for improved readability

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     @inertiaHead
     @vite(['resources/js/app.js', 'resources/js/css/quill-editor.css', 'resources/js/css/app.css'])
 </head>
+
 <body>
     @inertia
+    
     <audio id="audio">
         <source src="/api/stream" type="audio/mpeg">
     </audio>
+
     <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
     <script>
         window.OneSignalDeferred = window.OneSignalDeferred || [];
@@ -19,4 +23,5 @@
         });
     </script>
 </body>
+
 </html>
