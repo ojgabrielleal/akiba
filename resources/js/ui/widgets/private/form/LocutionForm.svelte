753e4c5c775536ea05c68a731d6fb630ed6a3e32
ojgabrielleal <joaogabrieel79@gmail.com>
2026-06-09 15:30:03 -0300

    refactor: improve layout structure in LocutionForm for better responsiveness and organization

--- resources/js/ui/widgets/private/form/LocutionForm.svelte
+++ resources/js/ui/widgets/private/form/LocutionForm.svelte
@@ -94,8 +94,8 @@
             placeholder="Digite a frase de locução"
             bind:value={$form.phrase.text}
         >
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 mt-5 lg:mt-12">
-            <div class="flex flex-wrap gap-2">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-5 lg:mt-12">
+            <div class="grid grid-cols-5 sm:grid-cols-10 gap-5">
                 {#each locutionDecorations as item}
                     <button
                         type="button"
@@ -118,11 +118,11 @@
                     </button>
                 {/each}
             </div>
-            <div class="flex flex-wrap justify-end gap-2">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
                 {#each locutionTextures as item}
                     <button
                         type="button"
-                        class={["cursor-pointer w-full lg:w-50 h-15 border-2 border-blue-ocean rounded-md disabled:cursor-not-allowed disabled:opacity-50", 
+                        class={["cursor-pointer h-15 border-2 border-blue-ocean rounded-md disabled:cursor-not-allowed disabled:opacity-50", 
                             { "border-blue-skywave drop-shadow-[0_0_20px_rgba(0,255,200,0.3)]": $form.phrase.texture === item.url },
                             { "border-blue-ocean": $form.phrase.texture !== item.url }
                         ]}
