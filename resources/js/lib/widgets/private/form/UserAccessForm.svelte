12db7d6afa4bcfb60f6d5c7a22e2d20c9b2e304d
ojgabrielleal <joaogabrieel79@gmail.com>
2026-07-31 16:04:07 -0300

    Remove realtime presence and fix access form footer

--- resources/js/lib/widgets/private/form/UserAccessForm.svelte
+++ resources/js/lib/widgets/private/form/UserAccessForm.svelte
@@ -23,35 +23,33 @@
     }
 </script>
 
-<form class="flex min-h-full flex-col" on:submit|preventDefault={submit}>
-    <div class="min-h-0 flex-1">
-        <FormField for="password" label="Nova senha" help="Essa senha será criptografada para proteção" error={$form.errors.password} spacing="compact">
-            <TextInput
-                variant="offcanvas"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗"
-                bind:value={$form.password}
-                error={$form.errors.password}
-            />
-        </FormField>
-        <SectionDivider tone="ocean">Cargos</SectionDivider>
-        <div class="pb-6">
-            <div class="flex flex-col gap-2">
-                {#if roles}
-                    {#each roles.data as item}
-                        <CheckboxInput
-                            label={item.label}
-                            description={item.description}
-                            name={item.name}
-                            id={item.name}
-                            value={item.name}
-                            bind:group={$form.roles}
-                        />
-                    {/each}
-                {/if}
-            </div>
+<form on:submit|preventDefault={submit}>
+    <FormField for="password" label="Nova senha" help="Essa senha será criptografada para proteção" error={$form.errors.password} spacing="compact">
+        <TextInput
+            variant="offcanvas"
+            id="password"
+            type="password"
+            name="password"
+            placeholder="∗∗∗∗∗∗∗∗∗∗∗∗∗∗∗"
+            bind:value={$form.password}
+            error={$form.errors.password}
+        />
+    </FormField>
+    <SectionDivider tone="ocean">Cargos</SectionDivider>
+    <div class="pb-6">
+        <div class="flex flex-col gap-2">
+            {#if roles}
+                {#each roles.data as item}
+                    <CheckboxInput
+                        label={item.label}
+                        description={item.description}
+                        name={item.name}
+                        id={item.name}
+                        value={item.name}
+                        bind:group={$form.roles}
+                    />
+                {/each}
+            {/if}
         </div>
     </div>
     {#if can.update}
